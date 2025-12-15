5a96647eeb36a590d59898da35fc030d36b3266b
Gourav Giri <codinggourav@gmail.com>
2025-12-15 10:09:48 +0530

    few change

--- src/main/java/com/substring/smartresult/entities/Student.java
+++ src/main/java/com/substring/smartresult/entities/Student.java
@@ -36,7 +36,7 @@ public class Student {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public String getRollNumber() { return rollNumber; }   // ✅ correct
+    public String getRollNumber() { return rollNumber; }
     public void setRollNumber(String rollNumber) { this.rollNumber = rollNumber; }
 
     public String getEmail() { return email; }
